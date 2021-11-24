55f08110e0b21f099e287963ce7140985278a892
Loai Nazeer <57464575+loaiwalid07@users.noreply.github.com>
2021-11-24 11:05:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -96,20 +96,20 @@ main_bg_ext = "jpg"
 side_bg = "Picture.png"
 side_bg_ext = "png"
 
-st.markdown(
-    f"""
-    <style>
-    .reportview-container {{
-        background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
+##st.markdown(
+ ##   f"""
+ ##   <style>
+ ##   .reportview-container {{
+ ##       background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
       
-    }}
-   .sidebar .sidebar-content {{
-        background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()})
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+##    }}
+##   .sidebar .sidebar-content {{
+##        background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()})
+##    }}
+##    </style>
+##    """,
+##    unsafe_allow_html=True
+##)
 ##
 
 
