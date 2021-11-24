0d738e2e6b313809908734b3a381ef8cb15f13e4
Loai Nazeer <57464575+loaiwalid07@users.noreply.github.com>
2021-11-24 11:56:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -101,8 +101,9 @@ st.markdown(
     <style>
     .stApp {{
        background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
-       background-size: contain;
+       
        background-repeat: no-repeat;
+      
 
     }}
    .sidebar .sidebar-content {{
