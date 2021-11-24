df2802bb6edabebdc69f722845c8c839a08212e1
Loai Nazeer <57464575+loaiwalid07@users.noreply.github.com>
2021-11-24 10:41:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -276,17 +276,21 @@ def visi (df,df_age,df_hash,df_em):
           st.image('1.png', width = 70)
           st.markdown(f"<h1 style='text-align: center;color: green;'>{round((values_s[item_s.index('positive')]/sum(values_s))*100)}%</h1>", unsafe_allow_html=True)
         except:
-          pass
+          st.image('1.png', width = 70)
+          st.markdown(f"<h1 style='text-align: center;color: green;'>{round((0*100)}%</h1>", unsafe_allow_html=True)
         try:
           st.image('2.png', width = 70)
           st.markdown(f"<h1 style='text-align: center;'>{round((values_s[item_s.index('neutral')]/sum(values_s))*100)}%</h1>", unsafe_allow_html=True)
         except:
-          pass
+          st.image('1.png', width = 70)
+          st.markdown(f"<h1 style='text-align: center;color: green;'>{round((0*100)}%</h1>", unsafe_allow_html=True)
         try:
           st.image('3.png', width = 70)
           st.markdown(f"<h1 style='text-align: center; color: red;'>{round((values_s[item_s.index('negative')]/sum(values_s))*100)}%</h1>", unsafe_allow_html=True)
         except:
-          st.markdown("***")
+          st.image('1.png', width = 70)
+          st.markdown(f"<h1 style='text-align: center;color: green;'>{round((0*100)}%</h1>", unsafe_allow_html=True)
+        st.markdown("***")
 ##############Word Cloud##################33
 
   with chart7:
